f1562fa1f7990a04e413a6400d0ee1779b8c6c30
tech88kubo <a_miss_oita@yahoo.co.jp>
2020-11-26 12:14:28 +0900

    Userモデルにバリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :user_name, presence: true
+  validates :profile, presence: true
+  validates :affiliation, presence: true
+  validates :position, presence: true
 end

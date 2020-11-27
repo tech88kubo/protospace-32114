791d5b2459e24271f9a4b46ba122e801f09a7562
tech88kubo <a_miss_oita@yahoo.co.jp>
2020-11-27 14:49:58 +0900

    User、Prototypeモデルにアソシエーション記述まで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:profile])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:affiliation])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:position])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name,:profile,:position,:affiliation])
   end
 end
